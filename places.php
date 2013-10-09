9e449178be53eb30a99ca2362be9b0f8be0f74d9
Federico Navarrete <fanmixco@gmail.com>
2013-10-09 02:17:58 -0600

    - Offline works.

--- places.php
+++ places.php
@@ -2,7 +2,7 @@
 	header("Content-Type: text/html; charset=utf-8");
 	require_once 'config.inc.php';
 	require_once 'Database.class.php';
-	$sql	= "SELECT idplace, p.idtype, s.name state, t.name type, p.name place, description, email, phone, website, extra, schedule, latitude, longitude, CONCAT('http://localhost/beach/images/places/',p.idplace, '.jpg') as image, vote FROM places p, states s, types t WHERE p.idstate=s.idstate AND p.idtype=t.idtype";
+	$sql	= "SELECT idplace, p.idtype, s.name state, t.name type, p.name place, description, email, phone, website, extra, schedule, latitude, longitude, CONCAT('/images/places/',p.idplace, '.jpg') as image, vote FROM places p, states s, types t WHERE p.idstate=s.idstate AND p.idtype=t.idtype";
 	if ($_GET)
 	{
 		if (@$_GET['idplace'])
--- types.php
+++ types.php
@@ -7,7 +7,7 @@
 	if ($_GET)
 		$sql	= "SELECT * FROM types WHERE idtype=".$_GET['idtype'];
 	else
-		$sql="SELECT (SELECT COUNT(idtype) from places where idtype=t.idtype) total, t.*, CONCAT('http://localhost/beach/images/type/',t.idtype, '.jpg') image FROM types t group by t.idtype having total>0";
+		$sql="SELECT (SELECT COUNT(idtype) from places where idtype=t.idtype) total, t.*, CONCAT('/images/type/',t.idtype, '.jpg') image FROM types t group by t.idtype having total>0";
 	$db = new Database('localhost', 'root', '', 'beach');
 	$db->connect();
 	echo $db->query2JSON($sql);
